53a26c7c91943a7fae0338e6a4f4fbc36dff2d57
Whoistolu <digittolu25@gmail.com>
2024-11-20 13:21:24 +0100

    Add urls to the navebar items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
+import React from 'react';
 import logo from '../assets/logo.png';
-import { FaLinkedin } from 'react-icons/fa';
-import { FaGithub } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
-import { FaXTwitter } from "react-icons/fa6";
-
+import { FaLinkedin, FaGithub, FaInstagram, FaXTwitter } from 'react-icons/fa6';
 
 const Navbar = () => {
   return (
-    <nav className='mb-20 flex items-center justify-between py-6'>
-        <div className="flex flex-shrink-0 items-center">
-            <img className='mx-2 w-12' src={logo} alt="logo" />
+    <nav className='flex items-center justify-between py-6 mb-20'>
+        <div className="flex items-center flex-shrink-0">
+            <img className='w-12 mx-2' src={logo} alt="logo" />
         </div>
-        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-            <FaLinkedin/>
-            <FaGithub/>
-            <FaInstagram/>
-            <FaXTwitter/>
+        <div className='flex items-center justify-center gap-4 m-8 text-2xl'>
+            <a href="https://www.linkedin.com/in/toluwase-ajise/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+                <FaLinkedin />
+            </a>
+            <a href="https://github.com/Whoistolu" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+                <FaGithub />
+            </a>
+            <a href="https://www.instagram.com/who_istolu/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                <FaInstagram />
+            </a>
+            <a href="https://x.com/Littletolu" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+                <FaXTwitter />
+            </a>
         </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
